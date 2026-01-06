f805a1ca6906f945b0b74c3b5c790a0885948b20
Gorkem387 <gorkemyildiz7ds@gmail.com>
2026-01-06 15:09:30 +0100

    page profil fonctionnel, on ne voit pas l'histortique des courses pour l'instant

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\CourseController;
 
@@ -29,6 +30,9 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    Route::get('/profile', [UserController::class, 'profile'])->name('user.profile');
+    Route::get('/profile/edit', [UserController::class, 'edit'])->name('user.edit');
+    Route::patch('/profile', [UserController::class, 'update'])->name('user.update');
     Route::get('/logs/{file}', function (string $file) {
   if ($file === 'laravel') {
     $content = Storage::disk('laravelLog')->get('laravel.log');
