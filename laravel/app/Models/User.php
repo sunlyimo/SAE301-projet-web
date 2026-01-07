e66e9fcbee8e37f527fa7fd345ee39aad18c6637
Oryx87 <baudin.mylan@laposte.net>
2026-01-07 15:59:01 +0100

    Add name club race

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -69,6 +69,24 @@ class User extends Authenticatable
         return $this->UTI_MOT_DE_PASSE;
     }
 
+    public function clubs()
+    {
+    return $this->belongsToMany(
+        Club::class,
+        'vik_responsable_club',
+        'UTI_ID',
+        'CLU_ID'
+    );
+    }
+
+    /**
+     * Relation avec le club via la table vik_coureur
+     */
+    public function clubAsCoureur()
+    {
+        return $this->hasOne(Coureur::class, 'UTI_ID', 'UTI_ID');
+    }
+
     /**
      * Vérifie si l'utilisateur est un administrateur
      */
