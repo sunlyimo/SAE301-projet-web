156089e9c3f6605cbef6e301107f85a2d7fe0181
Oryx87 <baudin.mylan@laposte.net>
2026-01-07 15:19:36 +0100

    Add space

--- laravel/resources/views/courses/index.blade.php
+++ laravel/resources/views/courses/index.blade.php
@@ -178,12 +178,12 @@
                             <div>
                                 <p class="text-xs text-gray-500 uppercase">Participants</p>
                                 <p class="font-black text-lg text-gray-800">0 <span class="text-xs font-normal text-gray-400">/ {{ $course->COU_PARTICIPANT_MAX }}</span></p>
-                                <p class="text-[14px] text-gray-400">Min: {{ $course->COU_PARTICIPANT_MIN }}</p>
+                                <p class="text-[14px] text-gray-400">Min : {{ $course->COU_PARTICIPANT_MIN }}</p>
                             </div>
                             <div>
                                 <p class="text-xs text-gray-500 uppercase">Équipes</p>
                                 <p class="font-black text-lg text-gray-800">0 <span class="text-xs font-normal text-gray-400">/ {{ $course->COU_EQUIPE_MAX }}</span></p>
-                                <p class="text-[14px] text-gray-400">Min: {{ $course->COU_EQUIPE_MIN }}</p>
+                                <p class="text-[14px] text-gray-400">Min : {{ $course->COU_EQUIPE_MIN }}</p>
                             </div>
                         </div>
                     </div>
