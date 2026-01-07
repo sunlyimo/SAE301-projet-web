010e006ddcf863dc6dff44f2a38474cb4cf96f8e
BouinY <bouin.yohan@outlook.fr>
2026-01-07 11:29:45 +0100

    Ajustement graphique sur le lien et le titre

--- laravel/resources/views/raid.blade.php
+++ laravel/resources/views/raid.blade.php
@@ -11,7 +11,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($raids as $raid)
                 <div class="bg-white rounded-3xl shadow-lg border border-gray-100 p-6 hover:shadow-2xl transition-all">
-                    <h2 class="font-extrabold"> {{ $raid->RAI_NOM }} </h2>
+                    <h4 class="font-extrabold"> {{ $raid->RAI_NOM }} </h4>
                     @if($raid->RAI_INSCRI_DATE_DEBUT <= now() && $raid->RAI_INSCRI_DATE_FIN >= now())
                         <p class="font-bold"> Inscription en cours</p>
                     @endif
@@ -24,7 +24,7 @@
                     <p class="font-bold"> {{ $raid->total_course }} courses</p> <!--Show Race count-->
                     <p>Déroulement du {{ $raid->RAI_RAID_DATE_DEBUT }} au {{ $raid->RAI_RAID_DATE_FIN}}</p>
                     <p>Inscription du {{ $raid->RAI_INSCRI_DATE_DEBUT }} au {{ $raid->RAI_INSCRI_DATE_FIN}}</p>
-                    <a href="/raids/{{$raid->RAI_ID}}" class="w-full mt-6 bg-black text-white py-3 rounded-xl font-bold hover:bg-green-600 transition-colors p-4">
+                    <a style="text-decoration: none !important;" href="/raids/{{$raid->RAI_ID}}" class="w-full mt-6 bg-black text-white py-3 rounded-xl font-bold hover:bg-green-600 transition-colors p-4">
                         Voir les courses
                     </a>
                 </div>
