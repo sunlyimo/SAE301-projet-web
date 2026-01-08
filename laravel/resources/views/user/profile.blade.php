f8e5c1c5a0816e7836e7e9dbdfdf30d28911a78e
Oryx87 <baudin.mylan@laposte.net>
2026-01-08 09:44:10 +0100

    Update

--- laravel/resources/views/user/profile.blade.php
+++ laravel/resources/views/user/profile.blade.php
@@ -268,7 +268,7 @@
                     {{-- Licence --}}
                     <div class="relative">
                         <label class="block text-sm font-semibold text-gray-700">Licence</label>
-                        <button type="button" onclick="toggleField('UTI_LICENCE')" class="absolute top-0 right-0 text-blacktext-black hover:text-green-600 transition-colors">
+                        <button type="button" onclick="toggleField('UTI_LICENCE')" class="absolute top-0 right-0 text-blacktext-black hover:text-green-600 transition-colors cursor-pointer">
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
                             </svg>
