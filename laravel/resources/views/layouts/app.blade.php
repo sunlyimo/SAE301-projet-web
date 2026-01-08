795c8b25054d6a5d725d3887241d245f4dd2b0a3
Oryx87 <baudin.mylan@laposte.net>
2026-01-08 10:49:18 +0100

    change comment

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -14,9 +14,9 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-20">
 
-                <!-- Logo + Bouton Menu Mobile -->
+                <!-- Logo + Mobile button -->
                 <div class="flex items-center gap-4">
-                    <!-- Bouton Menu Hamburger Mobile (maintenant à gauche) -->
+                    <!-- Mobile button (left)-->
                     <button @click="mobileMenuOpen = !mobileMenuOpen" class="md:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-green-600 hover:bg-gray-100 focus:outline-none transition-colors">
                         <svg class="h-6 w-6" :class="{'hidden': mobileMenuOpen, 'block': !mobileMenuOpen }" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
