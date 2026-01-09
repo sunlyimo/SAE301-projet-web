1d02fd7ce4d010327f11364fb6a9b5ed072ae4fe
Gorkem387 <gorkemyildiz7ds@gmail.com>
2026-01-09 10:35:04 +0100

    test app.blade

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CartoRun - @yield('title')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     @stack('styles')
 </head>
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
