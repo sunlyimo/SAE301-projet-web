76c5abab83dcfb2c9d1fd0692ec0babe8cc14ccb
Gorkem387 <gorkemyildiz7ds@gmail.com>
2026-01-09 10:39:03 +0100

    retour en arriere test

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CartoRun - @yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     @stack('styles')
 </head>
